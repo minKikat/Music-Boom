c22908a38408f1a6bf994cfc27d0c94560ce5e12
minyue1112 <minyue218@gmail.com>
2024-06-28 22:02:16 +0800

    sign up page

---
+++ music_app_boom/lib/signup.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Login(),
+        '/forgetPassword': (context) => ForgetPasswordScreen(),
+        '/signUp': (context) => SignUpScreen(),
+      },
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool _isObscure = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        color: Colors.black,
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/logo.jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              const Text(
+                'Create an account',
+                style: TextStyle(color: Colors.white, fontSize: 35),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.email, color: Colors.white),
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  labelText: 'Email',
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 20, 20, 20),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  labelStyle: const TextStyle(color: Colors.white),
+                ),
+                style: const TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 30), // 调整TextField和按钮之间的间距
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.lock, color: Colors.white),
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  labelText: 'Password',
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 20, 20, 20),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  labelStyle: const TextStyle(color: Colors.white),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                    icon: Icon(
+                      _isObscure ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                obscureText: _isObscure,
+                style: const TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 30), // 调整按钮和RichText之间的间距
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    // Add your login logic here
+                  },
+                  label: const Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 22),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 62, 200, 235),
+                    foregroundColor: Colors.white,
+                    fixedSize: const Size.fromHeight(50),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: "Already have account? ",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Login',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 62, 200, 235),
+                        decoration: TextDecoration.underline,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.pushNamed(context, '/signUp');
+                        },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ForgetPasswordScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forget Password'),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: const Text(
+          'Forget Password Screen',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: const Text(
+          'Sign Up Screen',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
