9749d7f1300096ec324091f518ac89addce34b59
WONGJIEYING <wongjieying0428@e.newera.edu.my>
2024-07-20 16:14:04 +0800

    myFavourite3

--- music_app_boom/lib/myFavourite3.dart
+++ music_app_boom/lib/myFavourite3.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class myFavourite3 extends StatefulWidget {
+  const myFavourite3({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<myFavourite3> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<myFavourite3> {
   double _sliderValue = 0.0;
 
   @override
@@ -126,7 +122,8 @@ class _MyAppState extends State<MyApp> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       IconButton(
-                        icon: const Icon(Icons.skip_previous, color: Colors.white),
+                        icon: const Icon(Icons.skip_previous,
+                            color: Colors.white),
                         onPressed: () {},
                       ),
                       Container(
