a57a05de816f1b21f4008e238c742de7a0029d1f
WONGJIEYING <wongjieying0428@e.newera.edu.my>
2024-07-20 16:04:17 +0800

    myFavourite2

--- music_app_boom/lib/myFavourite2.dart
+++ music_app_boom/lib/myFavourite2.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:music_app_boom/kpop.dart';
-//import 'package:flutter/widgets.dart';
 
-class Mandopop extends StatelessWidget {
-  const Mandopop({super.key});
+class myFavourite2 extends StatefulWidget {
+  const myFavourite2({super.key});
+
+  @override
+  State<myFavourite2> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<myFavourite2> {
+  double _sliderValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
