7a578cbbc850108770998317f136ed355bd8126a
WONGJIEYING <wongjieying0428@e.newera.edu.my>
2024-07-20 15:55:36 +0800

    Kpop2

--- music_app_boom/lib/kpop2.dart
+++ music_app_boom/lib/kpop2.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class Kpop2 extends StatefulWidget {
+  const Kpop2({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<Kpop2> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<Kpop2> {
   double _sliderValue = 0.0;
 
   @override
