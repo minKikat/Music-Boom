5e3f3ade5b635042c1f55e2ea5da3f75727ae516
WONGJIEYING <wongjieying0428@e.newera.edu.my>
2024-07-24 15:34:17 +0800

    kpop

--- music_app_boom/lib/kpop3.dart
+++ music_app_boom/lib/kpop3.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:music_app_boom/kpop.dart';
+import 'package:music_app_boom/kpop1.dart';
+import 'package:music_app_boom/kpop2.dart';
+import 'package:music_app_boom/kpop4.dart';
 
 class Kpop3 extends StatefulWidget {
   const Kpop3({super.key});
@@ -9,6 +13,21 @@ class Kpop3 extends StatefulWidget {
 
 class _MyAppState extends State<Kpop3> {
   double _sliderValue = 0.0;
+
+  bool isPlaying = false;
+  bool isFavorite = false;
+
+  void togglePlayPause() {
+    setState(() {
+      isPlaying = !isPlaying;
+    });
+  }
+
+  void toggleFavorite() {
+    setState(() {
+      isFavorite = !isFavorite;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +41,13 @@ class _MyAppState extends State<Kpop3> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.white),
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (BuildContext context) => const Kpop()));
+                    },
+                  ),
                   const Text(
                     'Bubble Gum',
                     style: TextStyle(
@@ -79,12 +105,17 @@ class _MyAppState extends State<Kpop3> {
                           inactiveColor: Colors.white,
                         ),
                       ),
-                      const Positioned(
-                        top: 0,
+                      Positioned(
+                        top: -10,
                         right: 10,
-                        child: Icon(
-                          Icons.favorite_border,
-                          color: Color.fromRGBO(57, 191, 212, 1),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.favorite_border,
+                            color: isFavorite
+                                ? Colors.red
+                                : Color.fromRGBO(57, 191, 212, 1),
+                          ),
+                          onPressed: toggleFavorite,
                         ),
                       ),
                     ],
@@ -122,8 +153,17 @@ class _MyAppState extends State<Kpop3> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       IconButton(
-                        icon: const Icon(Icons.skip_previous, color: Colors.white),
-                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.skip_previous,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      const Kpop2()));
+                        },
                       ),
                       Container(
                         decoration: const BoxDecoration(
@@ -132,14 +172,25 @@ class _MyAppState extends State<Kpop3> {
                           shape: BoxShape.circle,
                         ),
                         child: IconButton(
-                          icon: const Icon(Icons.play_arrow_outlined,
-                              color: Colors.white),
-                          onPressed: () {},
+                          icon: Icon(
+                            isPlaying ? Icons.pause : Icons.play_arrow_outlined,
+                            color: Colors.white,
+                          ),
+                          onPressed: togglePlayPause,
                         ),
                       ),
                       IconButton(
-                        icon: const Icon(Icons.skip_next, color: Colors.white),
-                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.skip_next,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      const Kpop4()));
+                        },
                       ),
                     ],
                   ),
